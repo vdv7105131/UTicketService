2263f66e150d5e228cf2763b3591b3e9b6d8760f
vdv7105131 <vdv7105131@gmail.com>
2022-04-06 08:59:00 +0300

    Добавьте файлы проекта.

---
+++ UTicketService.Tests/Calculator.Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UTicketService.Tests
+{
+    public class Calculator
+    {
+        public void MultiplcationMustReturnNotNull()
+        {
+            var calculator = new Calculator();
+            // Не получается вызвать метод Multiplication класса Calculator
+
+
+        }
+    }
+}
---
+++ UTicketService/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTicketService
+{
+    public class Calculator
+    {
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+
+
+
+}
